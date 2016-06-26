957d511ecef38dc7ee2923ae61a9ede0bda6d8f7
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2016-06-26 19:39:24 -0300

    Partial

--- src/parallel.c
+++ src/parallel.c
@@ -2,9 +2,10 @@
 #include <omp.h>
 #include <stdio.h>
 
-#define ROWS 1000
-#define COLUMNS 100000
-#define MIN(a,b) (((a)<(b))?(a):(b))
+#define COLUMNS 10
+#define ROWS 8
+#define SUICIDE_TAG 2
+#define WORK_TAG 1
 
 int matrix[ROWS][COLUMNS];
 
@@ -21,30 +22,76 @@ int compare(const void *a, const void *b) {
 	return (*(int *) a - *(int *) b);
 }
 
+void print_array(int array[]) {
+	int i;
+	for (i = 0; i < 10; ++i) {
+		printf("%d ", array[i]);
+	}
+	printf("\n");
+}
+
 int main(int argc, char *argv[]){
 	int my_rank;
 	int proc_n;
 	int omp_rank;
 	MPI_Status status;
 	int* x;
+	int i;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &proc_n);
-	#pragma omp parallel shared(my_rank) private(omp_rank)
-	{
-		omp_rank=omp_get_thread_num();
-		/*int y = ROWS/omp_get_thread_num();*/
-		if (my_rank == 0) {
-			populate_matrix();
-			printf("master -> rank: %d thread: %d \n",my_rank,omp_rank);
+
+	if (my_rank == 0){
+		int work_sent = ROWS;
+		int work_received = ROWS;
+		populate_matrix();
+
+		while(work_received > 0) {
+			for (i = 1; i < proc_n && work_sent > 0; ++i, work_sent-=2) {
+				MPI_Send(matrix[work_sent], COLUMNS*(proc_n-1),
+						MPI_INT, i, WORK_TAG,
+						MPI_COMM_WORLD);
+			}
+			for (i = 1; i < proc_n && work_received > 0; ++i, work_received-=2) {
+				MPI_Recv(matrix[work_received], COLUMNS*(proc_n-1),
+						MPI_INT, i, WORK_TAG,
+						MPI_COMM_WORLD, &status);
+				printf("Process number: %d -> ", i);
+				print_array(matrix[i]);
+				print_array(matrix[i+1]);
+				printf("\n");
+			}
 		}
-		else{
-			/*MPI_Recv(x, COLUMNS,*/
-					/*MPI_INT, 0, MPI_ANY_TAG,*/
-					/*MPI_COMM_WORLD, &status);*/
-			printf("slave -> rank: %d thread: %d \n",my_rank,omp_rank);
+
+		int terminator = proc_n;
+		while (--terminator) {
+			MPI_Send(0, 0, MPI_INT, terminator,
+					SUICIDE_TAG, MPI_COMM_WORLD);
+			
 		}
+	}
+	else{
+		int work_pool[(proc_n-1)][COLUMNS];
+		MPI_Recv(work_pool, COLUMNS*(proc_n-1),
+				MPI_INT, 0, MPI_ANY_TAG,
+				MPI_COMM_WORLD, &status);
+
+		if (status.MPI_TAG == SUICIDE_TAG) {
+			MPI_Finalize();
+			return 0;
+		}
+		#pragma omp parallel shared(work_pool, my_rank) private(omp_rank)
+		{
+			omp_rank=omp_get_thread_num();
+			qsort(work_pool[omp_rank], COLUMNS, sizeof(int), compare);
+			printf("my_rank: %d omp_rank: %d\n", my_rank, omp_rank);
+			print_array(work_pool[omp_rank]);
+		}
+		#pragma omp barrier
+		MPI_Send(work_pool, COLUMNS,
+				MPI_INT, 0, WORK_TAG,
+				MPI_COMM_WORLD);
 	}
 	MPI_Finalize();
 }
