a32482d4f054ef3bb0fdccbf2a22423ca173f064
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2016-06-27 18:14:21 -0300

    Updates

--- src/parallel.c
+++ src/parallel.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 #define COLUMNS 100
-#define ROWS 4
+#define ROWS 16
 #define SUICIDE_TAG 2
 #define WORK_TAG 1
 
@@ -47,40 +47,36 @@ int main(int argc, char *argv[]){
 	int proc_n;
 	int omp_rank;
 	int i;
+	int work = 0;
 	MPI_Status status;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &proc_n);
 
-	int work_size = ROWS * COLUMNS/(proc_n - 1);
-
 	if (my_rank == 0){
 		populate_matrix();
-		for (i = 0; i < proc_n - 1; ++i){
-			printf("work_size %d\n", work_size);
-			printf("matrix(%d)+i(%d)*work_size(%d): %d\n", matrix, i, work_size, matrix+(i*work_size));
-			MPI_Send(matrix+(i*work_size), work_size,
-					MPI_INT, i+1, WORK_TAG,
+		for (i = 1; i < proc_n; ++i){
+			printf("i: %d\n", i);
+			MPI_Send(matrix[i], 8*COLUMNS,
+					MPI_INT, i, WORK_TAG,
 					MPI_COMM_WORLD);
 		}
-		for (i = 0; i < proc_n - 1; ++i) {
-			MPI_Recv(matrix+(i*work_size), work_size,
-					MPI_INT, i+1, WORK_TAG,
+		for (i = 1; i < proc_n; ++i) {
+			MPI_Recv(matrix[i], 8*COLUMNS,
+					MPI_INT, i, WORK_TAG,
 					MPI_COMM_WORLD, &status);
 		}
 
 		int terminator = proc_n;
-		while (--terminator) {
-			MPI_Send(0, 0, MPI_INT, terminator,
-					SUICIDE_TAG, MPI_COMM_WORLD);
+		while (--terminator)
+			MPI_Send(0, 0, MPI_INT, terminator, SUICIDE_TAG, MPI_COMM_WORLD);
 
-		}
 		print_matrix();
 	}
 	else{
-		int work_pool[ROWS/(proc_n-1)][COLUMNS];
-		MPI_Recv(work_pool, work_size,
+		int work_pool[8][COLUMNS];
+		MPI_Recv(work_pool, 8*COLUMNS,
 				MPI_INT, 0, MPI_ANY_TAG,
 				MPI_COMM_WORLD, &status);
 
@@ -88,16 +84,13 @@ int main(int argc, char *argv[]){
 			MPI_Finalize();
 			return 0;
 		}
-		printf("row to sort: %d\n", work_size/COLUMNS);
 		#pragma omp parallel for
-		for (i = 0; i < ROWS/proc_n; ++i) {
+		for (i = 0; i < 8; ++i) {
 			qsort(work_pool[i], COLUMNS, sizeof(int), compare);
-			printf("print array: \n");
-			print_array(work_pool[i]);
 		}
 		#pragma omp barrier
 		printf("send!!!\n");
-		MPI_Send(work_pool, work_size,
+		MPI_Send(work_pool, 8*COLUMNS,
 				MPI_INT, 0, WORK_TAG,
 				MPI_COMM_WORLD);
 	}
