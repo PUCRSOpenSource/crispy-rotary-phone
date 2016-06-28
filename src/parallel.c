e50e1d10df5ae1c764d601f867ea1858b211153e
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2016-06-28 12:07:39 -0300

    Adds bubblesort

--- src/parallel.c
+++ src/parallel.c
@@ -24,6 +24,28 @@ int compare(const void *a, const void *b) {
 	return (*(int *) a - *(int *) b);
 }
 
+void bubble_sort(int size, int* array)
+{
+	int holder, swap = 1;
+	for (int i = 0; i < size-1 && swap; ++i)
+	{
+		swap = 0;
+		int limit = size - i -1;
+		for (int j = 0 ; j < limit; ++j)
+		{
+			int current = array[j];
+			int next = array[j + 1];
+			if (current > next)
+			{
+				holder     = current;
+				array[j]   = next;
+				array[j+1] = holder;
+				swap       = 1;
+			}
+		}
+	}
+}
+
 void print_array(int array[]) {
 	int i;
 	for (i = 0; i < 10; ++i) {
